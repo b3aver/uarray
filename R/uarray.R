df481e6b603dd629e1d2dda6e7f72fac9540712f
Paolo Sberna <reliablebeaver86-cs@yahoo.it>
2013-06-11 12:18:49 +0200

    First package files

---
+++ R/uarray.R
@@ -0,0 +1,26 @@
+#' Microarray data classification
+#' 
+#' Microarray data classification
+#'
+#' \describe{
+#'   \item{read}{Read a CSV file with tab separated fields}
+#'   \item{filter}{Filter genes}
+#'   \item{discretize}{Discretize gene expressions in intervals}
+#'   \item{train}{Train the classification model}
+#'   \item{classify}{Classify an entry}
+#' }
+#' 
+#' @references
+#' \describe{
+#'   \item{MIDClass}{Microarray Data Classification by Association Rules and Gene Expression Intervals}
+#'   \item{LIMMA}{Linear Models for Microarray Data}
+#'   \item{KEEL}{Knowledge Extraction based on Evolutionary Learning}
+#'  }
+#'
+#' @seealso \code{\link[limma]{limma}}
+#' 
+#' @author Paolo C. Sberna <reliablebeaver86-cs@@yahoo.it>
+#' @import limma
+#' @docType package
+#' @name uarray-package
+NULL
